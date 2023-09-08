fb42f3ef2a9fcb80f9ca88a708310cd824ee708c
Carlos DIaz <c.diazadriann@gmail.com>
2023-09-07 22:58:17 -0400

    Implementando el .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//Propio
 import 'package:teslo_shop/config/config.dart';
+import 'package:teslo_shop/config/constants/environment.dart';
 import 'package:teslo_shop/config/router/app_router.dart';
 
-void main() {
+void main() async {
+  //Aqui leo el archivo .env
+  await Enviroment.initEnvironment();
   runApp(const ProviderScope(child: MainApp()));
 }
 
@@ -12,6 +17,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(Enviroment.apiUrl);
     return MaterialApp.router(
       routerConfig: appRouter,
       theme: AppTheme().getTheme(),
